6b47cbe678b98b58d845973cbfd152b7793190fe
Chidera Nwankwo <danianne86@gmail.com>
2022-04-05 19:01:56 -0400

    second commit

---
+++ EmailFunction.js
@@ -0,0 +1,17 @@
+//Solution to Question 3
+
+let EvenGroup = [];
+ let OddGroup = [];
+ function UserID(numLoop) {
+     for (let i = 1; i <= numLoop; i++) {
+         if (i % 2 ===0) {
+             EvenGroup.push(i);
+         } else {
+             OddGroup.push(i)
+         }
+     }
+     console.log ("Total Number of EvenGroup UserID ="  + EvenGroup.length);
+     console.log ("Total Number of OddGroup UserID ="  + OddGroup.length);
+ }
+ UserID (100)
+ 
